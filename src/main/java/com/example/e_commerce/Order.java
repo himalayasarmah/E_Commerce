09e73fc3414ec4ebac09597a22c1fa2ea03765d2
himalayasarmah <121874173+himalayasarmah@users.noreply.github.com>
2023-12-29 20:03:18 +0530

    Update Order.java

--- src/main/java/com/example/e_commerce/Order.java
+++ src/main/java/com/example/e_commerce/Order.java
@@ -6,24 +6,23 @@ import java.sql.ResultSet;
 
 public class Order {
     public static boolean placeOrder(Customer customer, Product product) {
-        String groupOrderId = "select max(group_order_id) + 1 id from orders";
+        String groupOrderId = "SELECT max(group_order_id) + 1 id FROM orders";
         DbConnection dbConnection = new DbConnection();
 
-        try {
+        try{
             ResultSet rs = dbConnection.getQueryTable(groupOrderId);
-            if (rs.next()) {
-                String placeOrder = "insert into orders (group_order_id, customer_id, product_id) values (" +rs.getInt("id")+ ", " +customer.getId()+ ", " +product.getId()+ ")";
-//                String placeOrder = "insert into orders (group_order_id, customer_id, product_id) values('"+rs.getInt("id")+"', '"+customer.getId()+"', '"+product.getId()+")"'';
+            if(rs.next()){
+                String placeOrder = "insert into orders (group_order_id, customer_id, product_id) values ("+rs.getInt("id")+","+customer.getId()+","+product.getId()+")";
                 return dbConnection.updateDatabase(placeOrder) != 0;
             }
-        } catch (Exception e) {
+        }catch(Exception e){
             e.printStackTrace();
         }
         return false;
     }
 
     public static int placeMultipleOrder(Customer customer, ObservableList<Product> productList) {
-        String groupOrderId = "select max(group_order_id) + 1 id from orders";
+        String groupOrderId = "SELECT max(group_order_id) + 1 id FROM orders";
         DbConnection dbConnection = new DbConnection();
 
         try {
@@ -31,12 +30,12 @@ public class Order {
             int count = 0;
             if (rs.next()) {
                 for(Product product : productList){
-                    String placeOrder = "insert into orders (group_order_id, customer_id, product_id) values("+rs.getInt("id")+", "+customer.getId()+", "+product.getId()+")";
-                    count+= dbConnection.updateDatabase(placeOrder);
+                    String placeOrder = "insert into orders (group_order_id, customer_id, product_id) values ("+rs.getInt("id")+","+customer.getId()+","+product.getId()+")";
+                    count += dbConnection.updateDatabase(placeOrder);
                 }
                 return count;
             }
-        } catch (Exception e) {
+        }catch (Exception e) {
             e.printStackTrace();
         }
         return 0;
